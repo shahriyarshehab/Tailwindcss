2f269c2efef37c86050f50ac1011a1bb58509dce
shahriyarshehab <kshahriyar28@gmail.com>
2023-05-28 14:04:41 +0600

    update all

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tailwindcss
+# Tailwindcss
+# Tailwindcss
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./components/**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
